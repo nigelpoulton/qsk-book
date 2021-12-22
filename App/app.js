ac3359b5c95093583848e9cf324e2f4bd20b77fa
Nigel Poulton <nigelpoulton@hotmail.com>
2021-12-22 17:57:48 +0000

    Updating YAML and PUG files for 2022 edition

--- App/app.js
+++ App/app.js
@@ -1,5 +1,5 @@
 // Simple node.js web app for demonstrating containerizing apps
-// For quick demo purposes only (not properly maintained)
+// For quick demo purposes only (maintained approx annually)
 'use strict';
 
 var express = require('express'),
